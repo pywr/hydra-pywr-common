720db09eb1a8a0844bb8240a449a44c15193a61c
James Tomlinson <tomo.bbe@gmail.com>
2019-05-13 09:27:27 +0100

    Add PywrScenarios datatype.

--- hydra_pywr_common/datatypes.py
+++ hydra_pywr_common/datatypes.py
@@ -5,6 +5,10 @@ parameter_data_type_registry = {}
 recorder_data_type_registry = {}
 
 
+class PywrScenarios(PywrDataType):
+    tag = 'PYWR_SCENARIOS'
+
+
 class PywrPythonModule(PywrDataType):
     tag = 'PYWR_PY_MODULE'
 
@@ -25,7 +29,7 @@ class PywrParameter(PywrDataType):
             parameter_data_type_registry[cls.component.replace('parameter', '')] = cls
 
 
-class PywrParameterPattern(PywrParameter):
+class PywrParameterPattern(PywrDataType):
     tag = 'PYWR_PARAMETER_PATTERN'
     component = None
 
