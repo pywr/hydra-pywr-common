98c5fcdc0fedf23e5433eae948a9782da13debf8
knoxsp <knoxsp@users.noreply.github.com>
2019-05-28 16:35:11 +0100

    Give types readable names (#1)
    * Give types readable names

--- hydra_pywr_common/datatypes.py
+++ hydra_pywr_common/datatypes.py
@@ -7,19 +7,23 @@ recorder_data_type_registry = {}
 
 class PywrScenarios(PywrDataType):
     tag = 'PYWR_SCENARIOS'
+    name = 'Pywr Scenarios'
 
 
 class PywrPythonModule(PywrDataType):
     tag = 'PYWR_PY_MODULE'
+    name = 'Pywr Python Module'
 
 
 class PywrNodeOutput(PywrDataType):
     tag = 'PYWR_NODE_OUTPUT'
+    name = 'Pywr Node Output'
 
 
 class PywrParameter(PywrDataType):
     tag = 'PYWR_PARAMETER'
     component = 'parameter'
+    name = 'Pywr Parameter'
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
@@ -31,46 +35,55 @@ class PywrParameter(PywrDataType):
 
 class PywrParameterPattern(PywrDataType):
     tag = 'PYWR_PARAMETER_PATTERN'
+    name = 'Pywr Parameter Pattern'
     component = None
 
 
 class PywrParameterPatternReference(PywrParameter):
     tag = 'PYWR_PARAMETER_PATTERN_REF'
+    name = 'Pywr Parameter Pattern Reference'
     component = None
 
 
 class PywrDataframeParameter(PywrParameter):
     tag = 'PYWR_DATAFRAME'
+    name = 'Pywr Dataframe'
     component = 'dataframeparameter'
 
 
 class PywrMonthlyProfileParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_MONTHLY_PROFILE'
+    name = 'Pywr Parameter Monthly Profile'
     component = 'monthlyprofileparameter'
 
 
 class PywrControlCurveIndexParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_CONTROL_CURVE_INDEX'
+    name = 'Pywr Parameter Control Curve Index'
     component = 'controlcurveindexparameter'
 
 
 class PywrIndexedArrayParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_INDEXED_ARRAY'
+    name = 'Pywr Parameter Indexed Array'
     component = 'indexedarrayparameter'
 
 
 class PywrControlCurveInterpolatedParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_CONTROL_CURVE_INTERPOLATED'
+    name = 'Pywr Parameter Control Curve Interpolated'
     component = 'controlcurveinterpolatedparameter'
 
 
 class PywrAggregatedParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_AGGREGATED'
+    name = 'Pywr Parameter Aggregated'
     component = 'aggregatedparameter'
 
 
 class PywrConstantScenarioParameter(PywrParameter):
     tag = 'PYWR_PARAMETER_CONSTANT_SCENARIO'
+    name = 'Pywr Parameter Constant Scenario'
     component = 'constantscenarioparameter'
 
 
@@ -78,6 +91,7 @@ class PywrConstantScenarioParameter(PywrParameter):
 
 class PywrRecorder(PywrDataType):
     tag = 'PYWR_RECORDER'
+    name = 'Pywr Recorder'
     component = 'recorder'
 
     def __init_subclass__(cls, **kwargs):
@@ -90,14 +104,17 @@ class PywrRecorder(PywrDataType):
 
 class PywrFlowDurationCurveRecorder(PywrRecorder):
     tag = 'PYWR_RECORDER_FDC'
+    name = 'Pywr Recorder FDC'
     component = 'flowdurationcurverecorder'
 
 
 class PywrStorageDurationCurveRecorder(PywrRecorder):
     tag = 'PYWR_RECORDER_SDC'
+    name = 'Pywr Recorder SDC'
     component = 'storagedurationcurverecorder'
 
 
 class PywrFlowDurationCurveDeviationRecorder(PywrRecorder):
     tag = 'PYWR_RECORDER_FDC_DEVIATION'
+    name = 'Pywr Recorder FDC Deviation'
     component = 'flowdurationcurvedeviationrecorder'
