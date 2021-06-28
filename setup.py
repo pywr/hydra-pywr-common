e5e82570366643af726fa67dfa3928866f10f251
Paul Slavin <paul.slavin@manchester.ac.uk>
2021-06-28 19:29:36 +0100

    Appease setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hydra-pywr-common',
-    version='0.3',
+    version='0.4',
     description='Common data types and functions for Hydra and pywr integration.',
     packages=find_packages(include=['hydra_pywr_common', 'hydra_pywr_common.*', 'hydra_pywr_common.lib', 'hydra_pywr_common.lib.*']),
     include_package_data=True,
