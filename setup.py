053e2b48862bf4256eafe18992ab97b1ef606594
Paul Slavin <paul.slavin@manchester.ac.uk>
2021-06-28 19:07:56 +0100

    Lib in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='hydra-pywr-common',
-    version='0.1',
+    version='0.3',
     description='Common data types and functions for Hydra and pywr integration.',
-    packages=find_packages(),
+    packages=find_packages(include=['hydra_pywr_common', 'hydra_pywr_common.*', 'hydra_pywr_common.lib', 'hydra_pywr_common.lib.*']),
     include_package_data=True,
 )
